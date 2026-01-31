5a213c4383e0f9be607971d729145b57fd39aac6
uhhbads <abadshoodee@gmail.com>
2026-01-31 20:07:28 +0800

    connected controller and service

--- src/main/java/com/practice/SoftwareEngineerController.java
+++ src/main/java/com/practice/SoftwareEngineerController.java
@@ -10,19 +10,14 @@ import java.util.List;
 @RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getEngineers(){
-        return List.of(
-                new SoftwareEngineer(
-                        1,
-                        "James",
-                        "js, node, react, tailwindcss"
-                ),
-                new SoftwareEngineer(
-                        2,
-                        "Jamila",
-                        "java, spring, spring boot"
-                )
-        );
+        return softwareEngineerService.getSoftwareEngineers();
     }
 }
--- src/main/java/com/practice/SoftwareEngineerService.java
+++ src/main/java/com/practice/SoftwareEngineerService.java
@@ -2,6 +2,19 @@ package com.practice;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SoftwareEngineerService {
+    private final SoftwareEngineerRepository softwareEngineerRepository;
+
+    public SoftwareEngineerService(
+        SoftwareEngineerRepository softwareEngineerRepository
+    ){
+        this.softwareEngineerRepository = softwareEngineerRepository;
+    }
+
+    public List<SoftwareEngineer> getSoftwareEngineers(){
+        return softwareEngineerRepository.findAll();
+    }
 }

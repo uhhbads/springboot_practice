46943c03891a647f0b12fe038fe679e1c6ebf28d
uhhbads <abadshoodee@gmail.com>
2026-01-31 18:57:35 +0800

    added SE class

--- src/main/java/com/practice/SpringbootPracticeApplication.java
+++ src/main/java/com/practice/SpringbootPracticeApplication.java
@@ -1,13 +1,21 @@
 package com.practice;
 
+import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootPracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootPracticeApplication.class, args);
     }
 
+    @GetMapping
+    public String Hello(){
+        return "Hello";
+    }
 }
